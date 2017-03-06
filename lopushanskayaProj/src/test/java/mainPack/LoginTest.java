3c2adcf3256debe506077be4b648356364682005
Lopushanska <a.lopushanskaya@gmail.com>
2017-03-06 21:34:29 +0200

    COMMIT TO THE LOCAL BRANCH

--- lopushanskayaProj/src/test/java/mainPack/LoginTest.java
+++ lopushanskayaProj/src/test/java/mainPack/LoginTest.java
@@ -4,9 +4,6 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import parentTest.ParentTest;
 
-/**
- * Created by Admin on 01.03.2017.
- */
 public class LoginTest extends ParentTest {
     public LoginTest(String browser) {
         super(browser);
@@ -20,10 +17,19 @@ public class LoginTest extends ParentTest {
         loginPage.open("http://v3.test.itpmgroup.com");
         loginPage.checkTitle("Account of spare:Авторизация");
 
+
+        /**
         driver.findElement(By.name("_username")).clear();
         driver.findElement(By.name("_username")).sendKeys("Student");
-        driver.findElement(By.id("password")).clear();
-        driver.findElement(By.id("password")).sendKeys("909090");
+         driver.findElement(By.id("password")).clear();
+         driver.findElement(By.id("password")).sendKeys("909090");
+         loginPage.enterPassword("909090");
+        **/
+
+        loginPage.enterLogin("Student");
+        loginPage.enterPass("909090");
+
+
         driver.findElement(By.tagName("button")).click();
         checkAC("Avatar not present ", driver.findElement(By.xpath(".//div[@class='pull-left image']//img[@class='img-circle']")).isDisplayed(), true);
     }

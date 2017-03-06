8c364398b9e1dea2289a4270f1b0638e6ef568ff
Lopushanska <a.lopushanskaya@gmail.com>
2017-03-06 20:29:19 +0200

    COMMIT TO THE LOCAL BRANCH

--- lopushanskayaProj/src/test/java/parentTest/ParentTest.java
+++ lopushanskayaProj/src/test/java/parentTest/ParentTest.java
@@ -1,5 +1,6 @@
 package parentTest;
 
+import Pages.LoginPage;
 import libs.Utils;
 import org.apache.log4j.Logger;
 import org.junit.After;
@@ -33,10 +34,13 @@ public class ParentTest {
 
     private String pathToScreenShot;
     private String browser;
+    protected LoginPage loginPage;
 
 
     public ParentTest(String browser) {
+
         this.browser = browser;
+        loginPage = new LoginPage(driver);
     }
 
     @Parameterized.Parameters

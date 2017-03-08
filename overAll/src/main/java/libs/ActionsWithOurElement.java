56e0c2531a14daeb2fcc3ee3cd8bb8118ce285c0
Taras <radulenko@mail.ru>
2017-03-08 10:22:29 +0200

    Method isElementPresent was added

--- overAll/src/main/java/libs/ActionsWithOurElement.java
+++ overAll/src/main/java/libs/ActionsWithOurElement.java
@@ -8,34 +8,48 @@ import org.openqa.selenium.WebElement;
 
 
 public class ActionsWithOurElement {
-    WebDriver driver;
-    Logger logger;
+    private WebDriver driver;
+    private Logger logger;
 
-    public ActionsWithOurElement(WebDriver driver){
+    public ActionsWithOurElement(WebDriver driver) {
         this.driver = driver;
         logger = Logger.getLogger(getClass());
     }
 
-    public void enterText(String xpathLocator , String text){
-        try{
+    public void enterText(String xpathLocator, String text) {
+        try {
             driver.findElement(By.xpath(xpathLocator)).clear();
             driver.findElement(By.xpath(xpathLocator)).sendKeys(text);
             logger.info(text + " was inputed");
 
-        }catch (Exception e){
+        } catch (Exception e) {
             logger.error("Can not work with input");
             Assert.fail("Can not work with input");
         }
     }
 
     public void clickOnElement(String xpathLocator) {
-        try{
+        try {
             driver.findElement(By.xpath(xpathLocator)).click();
-            logger.info( "Element was clecked");
+            logger.info("Element was clecked");
 
-        }catch (Exception e){
+        } catch (Exception e) {
             logger.error("Can not work with button");
             Assert.fail("Can not work with button");
+        }
+    }
+
+    /**
+     * Method checked is element present on page
+     * @param xpathLocator
+     * @return
+     */
+    public boolean isElementPresent(String xpathLocator) {
+        try {
+            WebElement webElement = driver.findElement(By.xpath(xpathLocator));
+            return webElement.isDisplayed() && webElement.isEnabled();
+        } catch (Exception e) {
+            return false;
         }
     }
 }

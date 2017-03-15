25f13313fd1f3ae43d8792519e6f8c5c0fa0026c
kmelnik <melnik_ks@ukr.net>
2017-03-15 21:47:58 +0200

    wait + start new big test Workers

---
+++ melnykProj/src/main/java/Pages/WorkersPage.java
@@ -0,0 +1,11 @@
+package Pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+public class WorkersPage extends ParentPage {
+
+    public WorkersPage(WebDriver driver) {
+        super(driver);
+    }
+}

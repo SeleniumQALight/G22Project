9f37cc6c6393b87723de3758970b25f622f40bb7
Taras <radulenko@mail.ru>
2017-03-20 23:12:13 +0200

    Create Worker

--- overAll/src/main/java/Pages/WorkersPage.java
+++ overAll/src/main/java/Pages/WorkersPage.java
@@ -1,12 +1,38 @@
 package Pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
-/**
- * Created by QALight on 15.03.2017.
- */
+
 public class WorkersPage extends ParentPage{
+    @FindBy(xpath = ".//*[@href=\"http://v3.test.itpmgroup.com/dictionary/workers/edit\"]")
+    private WebElement buttonPlus;
+
+
     public WorkersPage(WebDriver driver) {
         super(driver);
+    }
+
+    public void clickButtonPlus() {
+        actionsWithOurElement.clickOnElement(buttonPlus);
+    }
+
+    /**
+     * Method fine worker by text in format
+     * WORKER_SURNAME +" " + WORKER_NAME + " " + WORKER_MIDDLE_NAME
+     * @param workerSurnameNameMiddleName
+     * @return
+     */
+    public Boolean isWorkerInList(String workerSurnameNameMiddleName) {
+        return actionsWithOurElement.isElementPresent(".//*[text()='"+workerSurnameNameMiddleName+"']");
+    }
+
+    public String getPhoneNumberForWorker(String workerSurnameNameMiddleName) {
+        return actionsWithOurElement.getTextFromElement(".//*[text()='"+workerSurnameNameMiddleName+"']//..//TD[2]");
+    }
+
+    public void clickOnWorker(String full_worker_name) {
+        actionsWithOurElement.clickOnElement(".//*[text()='"+full_worker_name+"']//..//TD[2]");
     }
 }

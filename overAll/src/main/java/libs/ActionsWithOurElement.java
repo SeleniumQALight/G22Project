5ed8e943c9473413a9db3c94506598e39190990a
Taras_Radulenko <Taras_Radulenko@epam.com>
2018-01-17 13:41:36 +0200

    some properties were added

--- overAll/src/main/java/libs/ActionsWithOurElement.java
+++ overAll/src/main/java/libs/ActionsWithOurElement.java
@@ -49,8 +49,7 @@ public class ActionsWithOurElement {
 
     public void clickOnElement(String xpathLocator) {
         try {
-            driver.findElement(By.xpath(xpathLocator)).click();
-            logger.info("Element was clecked");
+            clickOnElement(driver.findElement(By.xpath(xpathLocator)));
         } catch (Exception e) {
             printErrorAndStopTest();
         }

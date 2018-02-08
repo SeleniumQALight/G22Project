800f85291f24f7a0ed07e42b0ab67f269de9de42
Taras_Radulenko <Taras_Radulenko@epam.com>
2018-02-08 12:35:39 +0200

    POST  and SystemDate were added

--- overAll/src/test/java/restTest/TestRestAssured.java
+++ overAll/src/test/java/restTest/TestRestAssured.java
@@ -1,59 +1,69 @@
 package restTest;
 
 import RestSteps.MainRestSteps;
-import io.restassured.RestAssured;
-import io.restassured.http.Method;
-import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.response.ResponseBody;
 import io.restassured.specification.RequestSpecification;
 import libs.ResponseStructureForRestAssuredTest;
+import org.json.JSONObject;
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 
+import static libs.Utils.getDateAndTimeFormated;
 
 
 public class TestRestAssured {
+    MainRestSteps mainRestSteps = new MainRestSteps();
+
     @Test
-    public void GetWeatherDetails()
-    {
-        // Specify the base URL to the RESTful web service
-        RestAssured.baseURI = "http://restapi.demoqa.com/utilities/weather/city";
+    @Ignore
+    public void getWeatherDetails() {
 
-        // Get the RequestSpecification of the request that you want to sent
-        // to the server. The server is specified by the BaseURI that we have
-        // specified in the above step.
-        RequestSpecification httpRequest = RestAssured.given();
-        // Make a request to the server by specifying the method Type and the method URL.
-        // This will return the Response from the server. Store the response in a variable.
-        Response response = httpRequest.request(Method.GET, "/Hyderabad");
-        // Get the status code from the Response. In case of
-        // a successfull interaction with the web service, we
-        // should get a status code of 200.
-        int statusCode = response.getStatusCode();
+        Response response = mainRestSteps.getRequestAndVerifyStatusCode("/London");
 
-        // Assert that correct status code is returned.
-        Assert.assertEquals("Correct status code returned", statusCode /*actual value*/, 200 /*expected value*/);
+        ResponseBody responseBody = mainRestSteps.getResponseBody(response);
 
-        // Now let us print the body of the message to see what response
-        // we have recieved from the server
-        ResponseBody responseBody = response.getBody();
-        System.out.println("Response Body is =>  " + responseBody.asString());
-        // First get the JsonPath object instance from the Response interface
-        JsonPath jsonPathEvaluator = response.jsonPath();
-        // specified by JsonPath: City (Note: You should not put $. in the Java code)
-        String city = jsonPathEvaluator.get("City");
+        System.out.println("City received from Response " + mainRestSteps.getValueForKeyFromResponseAsJsonObject(response, "City"));
 
-        // Let us print the city variable to see what we got
-        System.out.println("City received from Response " + city);
-
-        // Validate the response
-        Assert.assertEquals("Correct city name received in the Response", city, "Hyderabad");
-
-        ResponseStructureForRestAssuredTest responseStructure = response.body().as(ResponseStructureForRestAssuredTest.class);
+        ResponseStructureForRestAssuredTest responseStructure = responseBody.as(ResponseStructureForRestAssuredTest.class);
 
         System.out.println("City from json " + responseStructure.City);
-
-
     }
+
+    @Test
+    public void registrationSuccessful() {
+        String dateTime = getDateAndTimeFormated();
+        RequestSpecification request = mainRestSteps.setBaseUrlForCustomer();
+
+        JSONObject requestParams = new JSONObject();
+        requestParams.put("FirstName", "Virender" + dateTime); // Cast
+        requestParams.put("LastName", "Singh" + dateTime);
+        requestParams.put("UserName", "sdimpleuser2dd20111" + dateTime);
+        requestParams.put("Password", "password");
+        requestParams.put("Email", "sample" + dateTime + "@gmail.com");
+
+        // Add a header stating the Request body is a JSON
+        request.header("Content-Type", "application/json");
+
+        request.body(requestParams.toMap());
+
+
+        Response response = request.post("/register");
+
+
+        int statusCode = response.getStatusCode();
+        Assert.assertEquals(201, statusCode);
+        ResponseBody responseBody = response.getBody();
+
+        System.out.println(response.asString());
+        if (response.jsonPath().get("SuccessCode") == null) {
+            String failCode = response.jsonPath().get("FaultId");
+            Assert.fail(failCode);
+        } else {
+            Assert.assertEquals("Correct Success code was returned", response.jsonPath().get("SuccessCode"), "OPERATION_SUCCESS");
+        }
+    }
+
+
 }
